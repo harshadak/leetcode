f29d15c5303dc4a33b275547d8c76e5dbc62b333
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2026-01-19 15:58:34 -0800

    341 - Flatten nested list iterator

---
+++ medium/341-flatten-nested-list-iterator.js
@@ -0,0 +1,79 @@
+/*
+Leetcode 341. Flatten Nested List Iterator
+https://leetcode.com/problems/flatten-nested-list-iterator/
+*/
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @constructor
+ * @param {NestedInteger[]} nestedList
+ */
+var NestedIterator = function (nestedList) {
+    this.stack = [...nestedList].reverse();
+};
+
+
+/**
+ * @this NestedIterator
+ * @returns {boolean}
+ */
+NestedIterator.prototype.hasNext = function () {
+    while (this.stack.length) {
+        const top = this.stack[this.stack.length - 1];
+
+        if (top.isInteger()) {
+            return true;
+        }
+
+        // top is a list
+        const list = this.stack.pop().getList(); // NestedInteger[]
+        // push in reverse so left-to-right order is preserved
+        for (let i = list.length - 1; i >= 0; i--) {
+            this.stack.push(list[i]);
+        }
+    }
+    return false;
+};
+
+/**
+ * @this NestedIterator
+ * @returns {integer}
+ */
+NestedIterator.prototype.next = function () {
+    if (!this.hasNext()) {
+        return undefined;
+    }
+
+    return this.stack.pop().getInteger();
+};
+
+/**
+ * Your NestedIterator will be called like this:
+ * var i = new NestedIterator(nestedList), a = [];
+ * while (i.hasNext()) a.push(i.next());
+*/
