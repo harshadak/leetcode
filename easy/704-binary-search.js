eddb729a5cfecc4e901204d540a1ef1bfcdbccd1
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2026-01-22 14:07:28 -0800

    704 - Binary search

---
+++ easy/704-binary-search.js
@@ -0,0 +1,21 @@
+/*
+    Leetcode 704. Binary Search
+    https://leetcode.com/problems/binary-search/
+*/
+
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((right - left) / 2) + left;
+
+        if (nums[mid] === target) return mid;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
