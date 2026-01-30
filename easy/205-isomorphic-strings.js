bfe3de83c5af12e768b819000ef46efa4298d753
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2026-01-29 16:41:32 -0800

    205 - Isomorphic strings

---
+++ easy/205-isomorphic-strings.js
@@ -0,0 +1,23 @@
+/*
+205. Isomorphic Strings
+https://leetcode.com/problems/isomorphic-strings/
+*/
+
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) return false;
+
+    const sToT = new Map();
+    const tToS = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        const sChar = s[i];
+        const tChar = t[i];
+
+        if (sToT.has(sChar) && (sToT.get(sChar) !== tChar)) return false;
+        if (tToS.has(tChar) && (tToS.get(tChar) !== sChar)) return false;
+
+        sToT.set(sChar, tChar);
+        tToS.set(tChar, sChar);
+    }
+    return true;
+};
