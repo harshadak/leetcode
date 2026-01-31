148a81801e2d73e9dda03a0d77ec29994e7d89b2
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2026-01-30 16:10:10 -0800

    69 - Sqrt(x)

---
+++ easy/69-sqrt(x).js
@@ -0,0 +1,25 @@
+/*
+    69. Sqrt(x)
+    https://leetcode.com/problems/sqrtx/
+*/
+
+var mySqrt = function (x) {
+    if (x < 2) return x;
+
+    let left = 1;
+    let right = x;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (mid * mid === x) {
+            return mid;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return right;
+};
