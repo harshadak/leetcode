8cd1d793d11d076ad08abc4adca66d194a3ea97a
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2026-05-19 23:42:16 -0700

    876 - Middle of the Linked List

---
+++ easy/876-middle-of-linked-list.js
@@ -0,0 +1,16 @@
+/*
+Leetcode Problem 876. Middle of the Linked List
+https://leetcode.com/problems/middle-of-the-linked-list
+*/
+
+var middleNode = function (head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+};
